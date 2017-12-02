7ed7c5c36e258dd2f0b94d7a99d9f657132347d6
Luke Spierewka <spierek@gmail.com>
2017-12-02 16:39:49 +0100

    Update

--- LD40/Assets/Code/GirlAI.cs
+++ LD40/Assets/Code/GirlAI.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 
@@ -88,7 +86,7 @@ public class GirlAI : CachedMonoBehaviour
         }
         else
         {
-            FollowPlayer(); 
+            FollowPlayer();
         }
 	}
 
