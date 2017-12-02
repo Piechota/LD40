a514aa7009b1b59039b507d93a04a1db327aefdf
piechota <piechota33@gmail.com>
2017-12-02 15:31:06 +0100

    girl cone

--- LD40/Assets/Code/GameManager.cs
+++ LD40/Assets/Code/GameManager.cs
@@ -1,6 +1,47 @@
-﻿public class GameManager : ASingleton<GameManager>
+﻿using UnityEngine;
+
+[System.Serializable]
+public class FieldOfView
 {
-	private PlayerController m_Player;
+    public int RaysNum;
+    public float RaysDistance;
+    public float ConeDegree;
+
+    public bool TestCollision(Vector3 position, Vector3 lookDir)
+    {
+        float playerRadius = GameManager.Instance.PlayerCollider.radius;
+        float coneDot = Mathf.Cos(ConeDegree * Mathf.Deg2Rad);
+        Vector3 playerPosition = GameManager.Instance.Player.transform.position;
+        Vector3 dir = playerPosition - position;
+        if (Vector3.Distance(playerPosition - dir.normalized * playerRadius, position) <= RaysDistance)
+        {
+            Vector3 perpendicularDir = new Vector3(dir.z, 0.0f, -dir.x);
+            perpendicularDir.Normalize();
+            Vector3 testPosition = playerPosition + perpendicularDir * playerRadius;
+
+            float delta = (2.0f * playerRadius) / (float)RaysNum;
+            for (int i = 0; i < RaysNum; ++i)
+            {
+                dir = testPosition - position;
+                dir.Normalize();
+
+                if (coneDot < Vector3.Dot(dir, lookDir))
+                {
+                    if (Physics.Raycast(position, dir, RaysDistance))
+                    {
+                        return true;
+                    }
+                }
+                testPosition -= perpendicularDir * delta;
+            }
+        }
+        return false;
+    }
+}
+
+public class GameManager : ASingleton<GameManager>
+{
+    private PlayerController m_Player;
 	public PlayerController Player
 	{
 		get
@@ -13,4 +54,19 @@
 			return m_Player;
 		}
 	}
+    private CapsuleCollider m_PlayerCollider;
+    public CapsuleCollider PlayerCollider
+    {
+        get
+        {
+            if (m_PlayerCollider == null)
+            {
+                m_PlayerCollider = Player.GetComponentInChildren<CapsuleCollider>();
+            }
+
+            return m_PlayerCollider;
+        }
+    }
+
+    public FieldOfView FieldOfView;
 }
---
+++ LD40/Assets/Code/GirlAI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlAI : CachedMonoBehaviour
+{
+    public Material ConeMaterial;
+    private bool Catched;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    private void DrawCone( Color color )
+    {
+        const int triangleNum = 20;
+
+        float deltaDegree = 2f * GameManager.Instance.FieldOfView.ConeDegree * Mathf.Deg2Rad / (float)triangleNum;
+        float cosDelta = Mathf.Cos(deltaDegree);
+        float sinDelta = Mathf.Sin(deltaDegree);
+
+        float coneCos = Mathf.Cos(-GameManager.Instance.FieldOfView.ConeDegree * Mathf.Deg2Rad);
+        float coneSin = Mathf.Sin(-GameManager.Instance.FieldOfView.ConeDegree * Mathf.Deg2Rad);
+
+        Vector3 initDir = new Vector3(coneSin * CachedTransform.forward.z + coneCos * CachedTransform.forward.x, 0f, coneCos * CachedTransform.forward.z - coneSin * CachedTransform.forward.x);
+        initDir.Normalize();
+        Vector3 pos0 = CachedTransform.position;
+        float coneRadius = GameManager.Instance.FieldOfView.RaysDistance;
+        GL.Begin(GL.TRIANGLES);
+        ConeMaterial.SetPass(0);
+        ConeMaterial.color = color;
+        GL.Color(Color.white);
+
+        for (int i = 0; i < triangleNum; ++i)
+        {
+            GL.Vertex3(pos0.x, pos0.y, pos0.z);
+            GL.Vertex3(pos0.x + initDir.x * coneRadius, pos0.y + initDir.y * coneRadius, pos0.z + initDir.z * coneRadius);
+
+            float x = sinDelta * initDir.z + cosDelta * initDir.x;
+            float z = cosDelta * initDir.z - sinDelta * initDir.x;
+            initDir.x = x;
+            initDir.z = z;
+
+            GL.Vertex3(pos0.x + initDir.x * coneRadius, pos0.y + initDir.y * coneRadius, pos0.z + initDir.z * coneRadius);
+        }
+        GL.End();
+    }
+    void OnRenderObject()
+    {
+        DrawCone(Catched ? Color.red : Color.blue);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        Catched = GameManager.Instance.FieldOfView.TestCollision(CachedTransform.position, CachedTransform.forward);
+	}
+}
