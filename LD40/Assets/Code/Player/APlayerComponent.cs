fcaf158548399ec1860decc847291fbd9914fae9
Luke Spierewka <spierek@gmail.com>
2017-12-02 15:11:22 +0100

    Player navigation

---
+++ LD40/Assets/Code/Player/APlayerComponent.cs
@@ -0,0 +1,35 @@
+﻿public abstract class APlayerComponent : CachedMonoBehaviour
+{
+	protected PlayerController m_Player;
+
+	public bool IsInitialized { get; private set; }
+
+	public void Initialize(PlayerController player)
+	{
+		if (IsInitialized)
+		{
+			Uninitialize();
+		}
+
+		m_Player = player;
+		HandleInitialization(player);
+		IsInitialized = true;
+	}
+
+	protected abstract void HandleInitialization(PlayerController player);
+
+	public virtual void UpdateBehaviour()
+	{
+	}
+
+	public void Uninitialize()
+	{
+		if (IsInitialized)
+		{
+			HandleUninitialization();
+			IsInitialized = false;
+		}
+	}
+
+	protected abstract void HandleUninitialization();
+}
---
+++ LD40/Assets/Code/Player/PlayerLocomotionController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerLocomotionController : APlayerComponent
+{
+	[Header("Movement")]
+	[SerializeField]
+	private float m_MovementSpeed = 4f;
+	[SerializeField]
+	private float m_Acceleration = 10f;
+	[SerializeField]
+	private float m_Deceleration = 15f;
+
+    private Vector3 m_CurrentVelocity;
+	private Vector3 m_TargetDirection;
+	private Vector3 m_TargetPosition;
+
+	private Vector3 m_PlayerScale;
+
+	private void Awake()
+	{
+	}
+
+	protected override void HandleInitialization(PlayerController player)
+	{
+	}
+
+	protected override void HandleUninitialization()
+	{
+	}
+
+	public override void UpdateBehaviour()
+	{
+		base.UpdateBehaviour();
+
+        m_TargetDirection = m_Player.Input.InputVector;
+
+		UpdateVelocity();
+		UpdateNavmesh();
+		UpdatePosition();
+
+		m_TargetDirection = Vector3.zero;    // for next turn
+	}
+
+	public void ResetVelocity()
+	{
+		m_CurrentVelocity = Vector3.zero;
+		m_TargetDirection = Vector3.zero;
+	}
+
+	public void ForceVelocity(Vector3 velocity)
+	{
+		m_CurrentVelocity = velocity;
+	}
+
+	public void AddVelocity(Vector3 velocity)
+	{
+		m_CurrentVelocity += velocity;
+	}
+
+	private void UpdateVelocity()
+	{
+		float targetSpeed = m_TargetDirection.magnitude * m_MovementSpeed;
+		Vector3 targetVelocity = m_TargetDirection * targetSpeed;
+
+		// check if we're accelerating or decelerating
+		bool isAccelerating = (targetSpeed >= m_CurrentVelocity.magnitude);
+		float acceleration = isAccelerating ? m_Acceleration : m_Deceleration;
+
+		// modify velocity
+		Vector3 velocityDiff = targetVelocity - m_CurrentVelocity;
+		m_CurrentVelocity += velocityDiff * acceleration * Time.deltaTime;
+	}
+
+	private void UpdateNavmesh()
+	{
+		Vector3 offset = m_CurrentVelocity * Time.deltaTime;
+		m_TargetPosition = CachedTransform.localPosition + offset;
+
+		NavMeshHit hit;
+		if (NavMesh.SamplePosition(m_TargetPosition, out hit, 1f, NavMesh.AllAreas))
+		{
+			m_TargetPosition = hit.position;
+		}
+	}
+
+	private void UpdatePosition()
+	{
+		CachedTransform.localPosition = m_TargetPosition;
+	}
+}
