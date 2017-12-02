b4a4a4f535328f84547c15e75693f1c2a22b7282
Luke Spierewka <spierek@gmail.com>
2017-12-02 16:29:39 +0100

    Rotating player model during movement

---
+++ LD40/Assets/Code/Player/PlayerAnimationController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimationController : APlayerComponent
+{
+	[SerializeField]
+	private Transform m_ModelTransform;
+
+	private Animator m_Animator;
+
+	private void Awake()
+	{
+		m_Animator = GetComponent<Animator>();
+	}
+
+	protected override void HandleInitialization(PlayerController player)
+	{
+	}
+
+	public override void UpdateBehaviour()
+	{
+		base.UpdateBehaviour();
+		SetOrientation(m_Player.Input.DirectionVector);
+	}
+
+	protected override void HandleUninitialization()
+	{
+	}
+
+	public void SetOrientation(Vector3 direction)
+	{
+		m_ModelTransform.forward = direction;
+	}
+}
