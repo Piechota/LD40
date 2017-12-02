02211f1bb043e7243fc18b616c2e1d718936fe4d
piechota <piechota33@gmail.com>
2017-12-02 14:42:52 +0100

    body colors

---
+++ LD40/Assets/Code/RandomBody.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBody : CachedMonoBehaviour {
+
+    [Header("Hair")]
+    [SerializeField]
+    private Renderer m_Hair;
+    [SerializeField]
+    private Gradient m_HairColors;
+
+    [Header("Body")]
+    [SerializeField]
+    private Renderer m_Body;
+    [SerializeField]
+    private Gradient m_BodyColors;
+
+    [Header("Skin")]
+    [SerializeField]
+    private Renderer m_Skin;
+    [SerializeField]
+    private Gradient m_SkinColors;
+
+    [Header("Eyes")]
+    [SerializeField]
+    private Renderer m_Eyes;
+    [SerializeField]
+    private Gradient m_EyesColors;
+
+    // Use this for initialization
+    void Start () {
+        m_Hair.material.color = m_HairColors.Evaluate(Random.value);
+        m_Body.material.color = m_BodyColors.Evaluate(Random.value);
+        m_Skin.material.color = m_SkinColors.Evaluate(Random.value);
+        m_Eyes.material.color = m_EyesColors.Evaluate(Random.value);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
