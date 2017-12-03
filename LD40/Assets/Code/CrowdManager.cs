db70d4a911e92d32ed4cc96f2d94db402fadcc99
piechota <piechota33@gmail.com>
2017-12-03 15:25:53 +0100

    clean up

--- LD40/Assets/Code/CrowdManager.cs
+++ LD40/Assets/Code/CrowdManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.AI;
 
 public class CrowdManager : ASingleton<CrowdManager>
 {
-    private List<NPC> m_NPCPool;
-
     [SerializeField]
     private GameObject m_NPCPrefab;
     [SerializeField]
@@ -16,61 +14,29 @@ public class CrowdManager : ASingleton<CrowdManager>
     public float LastDistanceOffset;
     public Vector2 MaxWaitTime;
     [SerializeField]
-    private int m_MaxNPC;
-    private int m_CurrentNPCNum = 0;
+    private int m_StartNPCNum;
     [SerializeField]
     private Vector2 m_DistanceFromPlayer;
 
-    public void AddNPCToPool( NPC npc )
+    public void SpawnNPC(int spawnNum)
     {
-        m_NPCPool.Add(npc);
-        npc.gameObject.SetActive(false);
-        --m_CurrentNPCNum;
+        Vector3 playerPosition = GameManager.Instance.Player.CachedTransform.position;
+        float distanceSpawnDelta = m_DistanceFromPlayer.y - m_DistanceFromPlayer.x;
+        for (int i = 0; i < spawnNum; ++i)
+        {
+            Vector3 dir = Random.onUnitSphere;
+            dir.y = 0f;
+            dir.Normalize();
+
+            Vector3 position = playerPosition + dir * (m_DistanceFromPlayer.x + Random.value * distanceSpawnDelta);
+            NPC npc = Instantiate(m_NPCPrefab).GetComponent<NPC>();
+
+            npc.Init(position);
+        }
     }
 
-    public void SpawnNPC()
-    {
-        if (m_MaxNPC <= m_CurrentNPCNum)
-        {
-            return;
-        }
-
-        Vector3 dir = Random.onUnitSphere;
-        dir.y = 0f;
-        dir.Normalize();
-
-        Vector3 position = GameManager.Instance.Player.CachedTransform.position + dir * ( m_DistanceFromPlayer.x + Random.value * (m_DistanceFromPlayer.y - m_DistanceFromPlayer.x) );
-
-        NPC npc = null;
-        int poolSize = m_NPCPool.Count;
-        if ( 0 < poolSize )
-        {
-            int index = Random.Range(0, poolSize);
-            npc = m_NPCPool[index];
-            m_NPCPool[index] = m_NPCPool[poolSize - 1];
-            m_NPCPool.RemoveAt(poolSize - 1);
-
-            npc.gameObject.SetActive(true);
-        }
-
-        if ( npc == null )
-        {
-            npc = Instantiate(m_NPCPrefab).GetComponent<NPC>();
-        }
-
-        ++m_CurrentNPCNum;
-        npc.Init(position);
-    }
-
-    public CrowdManager()
-    {
-        m_NPCPool = new List<NPC>();
-    }
     void Start()
     {
-        for ( int i = 0; i < m_MaxNPC; ++i)
-        {
-            SpawnNPC();
-        }
+        SpawnNPC(m_StartNPCNum);
     }
 }
