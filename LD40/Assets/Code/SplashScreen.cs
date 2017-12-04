ad257e52798d7c30e446165a72579f83f7338964
piechota <piechota33@gmail.com>
2017-12-05 00:48:57 +0100

    splash screen

---
+++ LD40/Assets/Code/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetButtonUp("Jump"))
+        {
+            SceneManager.LoadScene("main", LoadSceneMode.Single);
+        }
+	}
+}
